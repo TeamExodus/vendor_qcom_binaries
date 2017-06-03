7217f25f2d3e900c5c10e7e902309bc7756d0409
maxwen <max.weninger@gmail.com>
2017-06-03 16:35:53 +0200

    qcom: add add opt-out filter BOARD_USES_VENDOR_QCOM := false

--- Android.mk
+++ Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifneq ($(BOARD_USES_VENDOR_QCOM),false)
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include $(call all-makefiles-under,$(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM))
+endif
 endif
